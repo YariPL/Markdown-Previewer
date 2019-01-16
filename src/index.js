f7422b95fb7b1ad3a30cf81dae6865deddd2e272
Yari PL <yaroslav.khudchenko@gmail.com>
2019-01-16 23:47:14 +0100

    added 3 classes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,110 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+class App extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			textData: `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
+		}
+	this.handlechangeTextArea = this.handlechangeTextArea.bind(this);
+
+	}
+	handlechangeTextArea(){
+		console.log('ss');
+		this.setState({
+			textData:'s'
+		})
+	}
+	render() {
+		return (
+			<div>
+			<Editor text={this.state.textData}
+			/>
+			<Previewer text={this.state.textData}/>
+			</div>
+		)
+	}
+}
+
+export default App;
+
+
+class Editor extends React.Component {
+
+	
+	render() {
+		return(
+			<div className='editorField'>
+				<textarea type='text' defaultValue={this.props.text} className='editorFieldTextArea' onChange={this.props.handlechangeTextArea}/>
+			</div>
+		)
+	}
+}
+
+class Previewer extends React.Component {
+	render() {
+		return(
+			<div class='previewerField'>
+				<div class='previewerFieldText'>
+					{this.props.text}
+
+				</div>
+			</div>
+		)
+	}
+}
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
