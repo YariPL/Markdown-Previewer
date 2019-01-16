3c63fad16b74b8b759f4199f902248d370672c30
Yari PL <yaroslav.khudchenko@gmail.com>
2019-01-17 00:20:13 +0100

    made marked to work! needed to import it on top

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import marked from "marked";
 
 
 class App extends React.Component {
@@ -10,50 +11,6 @@ class App extends React.Component {
 		this.state = {
 			textData: `
 # Welcome to my React Markdown Previewer!
-
-## This is a sub-heading...
-### And here's some other cool stuff:
-  
-Heres some code, \`<div></div>\`, between 2 backticks.
-
-\`\`\`
-// this is multi-line code:
-
-function anotherExample(firstLine, lastLine) {
-  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-    return multiLineCode;
-  }
-}
-\`\`\`
-  
-You can also make text **bold**... whoa!
-Or _italic_.
-Or... wait for it... **_both!_**
-And feel free to go crazy ~~crossing stuff out~~.
-
-There's also [links](https://www.freecodecamp.com), and
-> Block Quotes!
-
-And if you want to get really crazy, even tables:
-
-Wild Header | Crazy Header | Another Header?
------------- | ------------- | ------------- 
-Your content can | be here, and it | can be here....
-And here. | Okay. | I think we get it.
-
-- And of course there are lists.
-  - Some are bulleted.
-     - With different indentation levels.
-        - That look like this.
-
-
-1. And there are numbererd lists too.
-1. Use just 1s if you want! 
-1. But the list goes on...
-- Even if you use dashes or asterisks.
-* And last but not least, let's not forget embedded images:
-
-![React Logo w/ Text](https://goo.gl/Umyytc)
 `
 		}
 	this.handlechangeTextArea = this.handlechangeTextArea.bind(this);
@@ -94,9 +51,9 @@ class Editor extends React.Component {
 class Previewer extends React.Component {
 	render() {
 		return(
-			<div class='previewerField'>
-				<div class='previewerFieldText'>
-					{this.props.text}
+			<div className='previewerField'>
+				<div className='previewerFieldText'>
+					{marked(this.props.text)};
 
 				</div>
 			</div>
