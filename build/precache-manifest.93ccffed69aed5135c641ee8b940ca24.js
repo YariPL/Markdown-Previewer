90fffc5617e6856a844f7eaee9104d8eaa43684b
Yari PL <yaroslav.khudchenko@gmail.com>
2019-01-17 23:42:41 +0100

    final dev ver

---
+++ build/precache-manifest.93ccffed69aed5135c641ee8b940ca24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ca160d9326a356d3162",
+    "url": "/static/js/main.7ca160d9.chunk.js"
+  },
+  {
+    "revision": "708dd8e6d03b7efafeae",
+    "url": "/static/js/1.708dd8e6.chunk.js"
+  },
+  {
+    "revision": "7ca160d9326a356d3162",
+    "url": "/static/css/main.0c9aad46.chunk.css"
+  },
+  {
+    "revision": "01e1597cdea47a161795049b40c98532",
+    "url": "/index.html"
+  }
+];
